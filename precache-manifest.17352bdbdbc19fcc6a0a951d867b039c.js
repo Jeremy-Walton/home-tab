a162165abf3434511938611199e76bda8a81f5a7
Jeremy Walton <jthelegoman@gmail.com>
2021-04-17 18:00:51 -0400

    Release 1.8.3

---
+++ precache-manifest.17352bdbdbc19fcc6a0a951d867b039c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160bb935129ba8abf53a6d2281fd0c2f",
+    "url": "/home-tab/index.html"
+  },
+  {
+    "revision": "9a8a501a027ae6385c03",
+    "url": "/home-tab/static/css/main.87b629b2.chunk.css"
+  },
+  {
+    "revision": "d3574d0cdc89fb01ec3a",
+    "url": "/home-tab/static/js/2.4d8b4fd7.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/home-tab/static/js/2.4d8b4fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8a501a027ae6385c03",
+    "url": "/home-tab/static/js/main.beb85bda.chunk.js"
+  },
+  {
+    "revision": "7bb39a46a1ad9f2eb6b2",
+    "url": "/home-tab/static/js/runtime-main.99406c88.js"
+  },
+  {
+    "revision": "b13e6268396417d98199eb3468cd0bda",
+    "url": "/home-tab/static/media/Anton-Regular.b13e6268.ttf"
+  }
+]);
