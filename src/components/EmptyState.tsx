7154be11d82418f08581d0d94f396758b37d649e
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2026-06-21 22:12:47 -0400

    Use card for empty state

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,19 +1,21 @@
 import { PlusIcon } from '@phosphor-icons/react'
 import { Button } from './ui/button'
-import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyTitle } from './ui/empty'
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
 
 export function EmptyState({ onAddLink }: { onAddLink: () => void }) {
   return (
-    <Empty className="flex-1">
-      <EmptyHeader>
-        <EmptyTitle>Welcome to Launch Tabs!</EmptyTitle>
-        <EmptyDescription>Add your first link to get started.</EmptyDescription>
-      </EmptyHeader>
-      <EmptyContent>
-        <Button onClick={onAddLink}>
-          <PlusIcon /> Add link
-        </Button>
-      </EmptyContent>
-    </Empty>
+    <div className="flex flex-1 items-center justify-center">
+      <Card className="w-80 bg-card/90">
+        <CardHeader>
+          <CardTitle>Welcome to Launch Tabs!</CardTitle>
+          <CardDescription>Add your first link to get started.</CardDescription>
+        </CardHeader>
+        <CardFooter className="border-t">
+          <Button className="w-full" onClick={onAddLink}>
+            <PlusIcon /> Add link
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
